dca83424ec33ba273b109848c36f6103c7650614
GoodGuyGregory <greg.witt625@gmail.com>
2023-09-02 18:48:11 -0700

    removes deprecated bodyparser calls

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const movieRouter = require('./routes/movie-router')
 const app = express();
 const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
-app.use(bodyParser.json());
+
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
